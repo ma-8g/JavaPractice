1ebb03ca5b899de0549f1d5a60711da0c5e8f24c
Kenichiro Takarada <78543986+ma-8g@users.noreply.github.com>
2025-03-02 17:15:18 +0900

    codeパッケージを追加しプログラムファイルを移動

--- src/code/Operation.java
+++ src/code/Operation.java
@@ -1,57 +1,41 @@
+package code;
 
-public class OperationWithArgs {
+public class Operation {
 
 	public static void main(String[] args) {
 		double x = 4;
 		double y = 2;
 
 		for (EnumOperation op : EnumOperation.values()) {
-			System.out.printf("%d: %s (%s) %f %s %f = %f%n", op.getNumber(), op, op.getName(), x, op, y,
-					op.calculate(x, y));
+			System.out.printf("%f %s %f = %f%n", x, op, y, op.calculate(x, y));
 		}
 	}
 
 	enum EnumOperation {
-		ADD(1, "足し算") {
+		ADD {
 			@Override
 			public double calculate(double x, double y) {
 				return x + y;
 			}
 		},
-		SUBTRACT(2, "引き算") {
+		SUBTRACT {
 			@Override
 			public double calculate(double x, double y) {
 				return x - y;
 			}
 		},
-		MULTIPLY(3, "掛け算") {
+		MULTIPLY {
 			@Override
 			public double calculate(double x, double y) {
 				return x * y;
 			}
 		},
-		DIVIDE(4, "割り算") {
+		DIVIDE {
 			@Override
 			public double calculate(double x, double y) {
 				return x / y;
 			}
 		};
-
-		private final int number;
-		private final String name;
-
-		EnumOperation(int number, String name) {
-			this.number = number;
-			this.name = name;
-		}
-
-		public int getNumber() {
-			return number;
-		}
-
-		public String getName() {
-			return name;
-		}
 
 		public abstract double calculate(double x, double y);
 	}
--- src/code/OperationWithArgs.java
+++ src/code/OperationWithArgs.java
@@ -1,40 +1,62 @@
+package code;
 
-public class Operation {
+public class OperationWithArgs {
 
 	public static void main(String[] args) {
 		double x = 4;
 		double y = 2;
 
 		for (EnumOperation op : EnumOperation.values()) {
-			System.out.printf("%f %s %f = %f%n", x, op, y, op.calculate(x, y));
+			System.out.printf("%d: %s (%s) %f %s %f = %f%n", op.getNumber(), op, op.getName(), x, op, y,
+					op.calculate(x, y));
+		}
+
+		if (EnumOperation.ADD.getNumber() == 1) {
+
 		}
 	}
 
 	enum EnumOperation {
-		ADD {
+		ADD(1, "足し算") {
 			@Override
 			public double calculate(double x, double y) {
 				return x + y;
 			}
 		},
-		SUBTRACT {
+		SUBTRACT(2, "引き算") {
 			@Override
 			public double calculate(double x, double y) {
 				return x - y;
 			}
 		},
-		MULTIPLY {
+		MULTIPLY(3, "掛け算") {
 			@Override
 			public double calculate(double x, double y) {
 				return x * y;
 			}
 		},
-		DIVIDE {
+		DIVIDE(4, "割り算") {
 			@Override
 			public double calculate(double x, double y) {
 				return x / y;
 			}
 		};
+
+		private final int number;
+		private final String name;
+
+		EnumOperation(int number, String name) {
+			this.number = number;
+			this.name = name;
+		}
+
+		public int getNumber() {
+			return number;
+		}
+
+		public String getName() {
+			return name;
+		}
 
 		public abstract double calculate(double x, double y);
 	}
